30c6eab72a0b80ba059a6b8b368909fda5c1f6ec
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-14 12:56:00 +0530

    Time: 29 ms (68.50%), Space: 13.4 MB (44.79%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -8,26 +8,64 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-typedef pair<int,ListNode*> pr;
+// typedef pair<int,ListNode*> pr;
+// class Solution {
+// public:
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         ListNode* ans = new ListNode();
+//         ListNode* curr=ans;
+//         priority_queue<pr,vector<pr>,greater<pr>>pq;
+//         for(auto list: lists){
+//             if(list!=NULL){
+//                 pq.push({list->val,list});
+//             }
+//         }
+//         while(!pq.empty()){
+//             curr->next=pq.top().second;
+//             pq.pop();
+//             curr=curr->next;
+//             if(curr->next){
+//                 pq.push({curr->next->val,curr->next});
+//             }
+//         }
+//         return ans->next;
+//     }
+// };
+
 class Solution {
+private:
+    ListNode* merge2Lists(ListNode* first, ListNode* second){
+        ListNode* head= new ListNode();
+        ListNode * curr=head;
+        while(first && second){
+            if(first->val<second->val){
+                curr->next=first;
+                first=first->next;
+            }else{
+                curr->next=second;
+                second=second->next;
+            }
+            curr=curr->next;
+        }
+        if(first)
+            curr->next=first;
+        else
+            curr->next=second;
+        return head->next;
+    }
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* ans = new ListNode();
-        ListNode* curr=ans;
-        priority_queue<pr,vector<pr>,greater<pr>>pq;
-        for(auto list: lists){
-            if(list!=NULL){
-                pq.push({list->val,list});
+        int n=lists.size();
+        if(n==0)
+            return NULL;
+        int gap=1;
+        while(gap<n){
+            for(int i=0;i<n;i+=(2*gap)){
+                if(i+gap<n)
+                    lists[i]=merge2Lists(lists[i],lists[i+gap]);
             }
+            gap*=2;
         }
-        while(!pq.empty()){
-            curr->next=pq.top().second;
-            pq.pop();
-            curr=curr->next;
-            if(curr->next){
-                pq.push({curr->next->val,curr->next});
-            }
-        }
-        return ans->next;
+        return lists[0];
     }
 };
