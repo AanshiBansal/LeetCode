078cf499d7074b77edf2ef60d7a5d471eadf177f
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-15 19:49:07 +0530

    Time: 100 ms (71.55%), Space: 68.5 MB (95.33%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.cpp
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int minSwaps(vector<int>& data) {
         int n=data.size();
-        int ans=INT_MAX;
+        int maxones=0;
         int count=0;
         for(int d:data){
             if(d==1)
                 count++;
         }
-        if(count==0)
-            return 0;
-        for(int i=1;i<n;i++)
-            data[i]+=data[i-1];
-        for(int i=0;i+count<n;i++){
-            ans=min(ans,count-(data[i+count]-data[i]));
+        int start=0;
+        int end=0;
+        int curr=0;
+        while(end<n){
+            curr+=data[end++];
+            if(end-start>count){
+                curr-=data[start++];
+            }
+            maxones=max(maxones,curr);
         }
-        return min(ans,count-data[count-1]);
+        return count-maxones;
     }
 };
