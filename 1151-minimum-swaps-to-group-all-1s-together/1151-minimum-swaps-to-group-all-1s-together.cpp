c26de90838bb731522411c9deb2286e23376a2a5
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-15 19:54:02 +0530

    Time: 107 ms (63.19%), Space: 68.7 MB (28.34%) - LeetHub

--- 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.cpp
+++ 1151-minimum-swaps-to-group-all-1s-together/1151-minimum-swaps-to-group-all-1s-together.cpp
@@ -4,19 +4,17 @@ public:
         int n=data.size();
         int maxones=0;
         int count=0;
-        for(int d:data){
-            if(d==1)
-                count++;
-        }
-        int start=0;
+        for(int d:data)
+            count+=d;
         int end=0;
         int curr=0;
         while(end<n){
-            curr+=data[end++];
-            if(end-start>count){
-                curr-=data[start++];
+            curr+=data[end];
+            if(end>=count){
+                curr-=data[end-count];
             }
             maxones=max(maxones,curr);
+            end++;
         }
         return count-maxones;
     }
