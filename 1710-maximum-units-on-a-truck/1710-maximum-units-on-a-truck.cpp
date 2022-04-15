cd3759fec0e0c6cad71bb0399bd8987ed2da7c16
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-15 12:04:21 +0530

    Time: 25 ms (99.77%), Space: 16.3 MB (52.84%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -3,14 +3,28 @@ bool comparator(vector<int>&a,vector<int>&b){
 }
 class Solution {
 public:
+    // int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+    //     sort(boxTypes.begin(),boxTypes.end(),comparator);
+    //     int ans=0;
+    //     int i=0;
+    //     while(i<boxTypes.size() && truckSize>0){
+    //         ans+=min(boxTypes[i][0],truckSize)*boxTypes[i][1];
+    //         truckSize-=boxTypes[i][0];
+    //         i++;
+    //     }
+    //     return ans;
+    // }
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(),boxTypes.end(),comparator);
         int ans=0;
-        int i=0;
-        while(i<boxTypes.size() && truckSize>0){
-            ans+=min(boxTypes[i][0],truckSize)*boxTypes[i][1];
-            truckSize-=boxTypes[i][0];
-            i++;
+        vector<int>count(1001,0);
+        for(int i=0;i<boxTypes.size();i++){
+            count[boxTypes[i][1]]+=boxTypes[i][0];
+        }
+        int i=1000;
+        while(i>0 && truckSize>0){
+            ans+=min(count[i],truckSize)*i;
+            truckSize-=count[i];
+            i--;
         }
         return ans;
     }
