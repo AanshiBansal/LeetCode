d381a25f2f597ee3fa8af8a616c54ac5c8398c41
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-16 15:31:00 +0530

    Time: 27 ms (32.96%), Space: 14.6 MB (25.77%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    bool rec(string s, int index, int n, unordered_set<string>&dict, vector<int>&mp){
+        if(index==n)
+            return true;
+        if(mp[index]==0)
+            return false;
+        if(mp[index]==1)
+            return true;
+        for(int i=index;i<n;i++){
+            if(dict.find(s.substr(index,i-index+1))!=dict.end() && rec(s,i+1,n,dict,mp)){
+                mp[index]=1;
+                return true;
+            }
+        }
+        mp[index]=0;
+        return false;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string>dict;
+        for(string s:wordDict){
+            dict.insert(s);
+        }
+        int n=s.size();
+        vector<int>mp(n,2);
+        return rec(s,0,n,dict,mp);
+    }
+};
