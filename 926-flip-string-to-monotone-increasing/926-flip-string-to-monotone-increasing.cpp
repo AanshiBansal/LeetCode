6dbc1da81a070f5a1b073ec72733a591e43e47e2
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-22 05:27:20 +0530

    Time: 31 ms (68.29%), Space: 11.1 MB (52.90%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,29 +1,51 @@
+// class Solution {
+// public:
+//     int minFlipsMonoIncr(string s) {
+//         int n=s.size();
+//         if(n==0)
+//             return 0;
+//         vector<int>one(n+1);
+//         one[n]=0;
+//         if(s[n-1]=='1'){
+//             one[n-1]=0;
+//         }else{
+//             one[n-1]=1;
+//         }
+//         for(int i=n-2;i>=0;i--){
+//             if(s[i]=='1'){
+//                 one[i]=one[i+1];
+//             }else{
+//                 one[i]=one[i+1]+1;
+//             }
+//         }
+//         int ans=one[0];
+//         int zero=0;
+//         for(int i=0;i<n;i++){
+//             if(s[i]=='1')
+//                 zero++;
+//             ans=min(zero+one[i+1],ans);
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
         int n=s.size();
-        if(n==0)
-            return 0;
-        vector<int>one(n+1);
-        one[n]=0;
-        if(s[n-1]=='1'){
-            one[n-1]=0;
-        }else{
-            one[n-1]=1;
-        }
-        for(int i=n-2;i>=0;i--){
-            if(s[i]=='1'){
-                one[i]=one[i+1];
-            }else{
-                one[i]=one[i+1]+1;
-            }
-        }
-        int ans=one[0];
-        int zero=0;
+        int ones=0;
+        int ans=0;
         for(int i=0;i<n;i++){
-            if(s[i]=='1')
-                zero++;
-            ans=min(zero+one[i+1],ans);
+            if(s[i]=='1'){
+                ones++;
+            }else{
+                // if(ones<=ans+1){
+                //     ans=ones;
+                // }else{
+                //     ans++;
+                //     ones++;
+                // }
+                ans=min(ans+1,ones);
+            }
         }
         return ans;
     }
