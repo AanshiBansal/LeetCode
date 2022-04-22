f5ea5be3dbfc21fa8705ce213c776c157befc24f
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-04-22 13:30:21 +0530

    Time: 13 ms (73.58%), Space: 17.8 MB (74.81%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,17 +1,40 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int ans=0;
+//         int n=height.size();
+//         vector<int>right_max(n);
+//         right_max[n-1]=0;
+//         for(int i=n-2;i>=0;i--){
+//             right_max[i]=max(right_max[i+1],height[i+1]);
+//         }
+//         int left_max=0;
+//         for(int i=0;i<n;i++){
+//             ans+=max(min(left_max,right_max[i])-height[i],0);
+//             left_max=max(left_max,height[i]);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int trap(vector<int>& height) {
         int ans=0;
-        int n=height.size();
-        vector<int>right_max(n);
-        right_max[n-1]=0;
-        for(int i=n-2;i>=0;i--){
-            right_max[i]=max(right_max[i+1],height[i+1]);
-        }
         int left_max=0;
-        for(int i=0;i<n;i++){
-            ans+=max(min(left_max,right_max[i])-height[i],0);
-            left_max=max(left_max,height[i]);
+        int right_max=0;
+        int i=0;
+        int j=height.size()-1;
+        while(i<=j){
+            if(left_max<right_max){
+                ans+=max(left_max-height[i],0);
+                left_max=max(left_max,height[i]);
+                i++;
+            }else{
+                ans+=max(right_max-height[j],0);
+                right_max=max(right_max,height[j]);
+                j--;
+            }
         }
         return ans;
     }
