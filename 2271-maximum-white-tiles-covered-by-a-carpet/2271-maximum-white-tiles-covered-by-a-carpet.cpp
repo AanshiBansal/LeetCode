1fa8c84510a1a3816bb519fff81fb7ee5d64b60a
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-15 00:53:34 +0530

    Time: 406 ms (50.00%), Space: 69.5 MB (100.00%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -3,27 +3,26 @@ public:
     int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
         sort(tiles.begin(),tiles.end());
         int n=tiles.size();
-        int prev=0;
-        int carpetStart=tiles[0][0];
         int ans=0;
-        int l=0;
         int curr=0;
-        int prevEnding=tiles[0][0]-1;
+        int end=0;
         for(int i=0;i<n;i++){
-            curr+=(tiles[i][1]-tiles[i][0]+1);
-            l+=(tiles[i][1]-prevEnding);
-            prevEnding=tiles[i][1];
-            while(l>carpetLen){
-                l--;
-                if(carpetStart<=tiles[prev][1]);
-                else
-                    prev++;
-                if(carpetStart>=tiles[prev][0]){
-                    curr--;
-                }
-                carpetStart++;
+            if(i>0){
+                curr-=(tiles[i-1][1]-tiles[i-1][0]+1);
             }
-            ans=max(ans,curr);
+            int temp=curr;
+            while(end<n && tiles[end][0]-tiles[i][0]<=carpetLen){
+                int l=(tiles[end][1]-tiles[i][0]+1);
+                if(l<=carpetLen){
+                    curr+=(tiles[end][1]-tiles[end][0]+1);
+                    temp=curr;
+                    end++;
+                }else{
+                    temp+=(carpetLen-(tiles[end][0]-tiles[i][0]));
+                    break;
+                }
+            }
+            ans=max(ans,temp);
         }
         return ans;
     }
