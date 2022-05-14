20d4c210313dc67abf9ba952fec930f67a69270a
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-14 23:13:49 +0530

    Time: 2 ms (50.00%), Space: 6.1 MB (16.67%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.cpp
@@ -6,18 +6,19 @@ public:
             return 0;
         int ans=0;
         int curr=0;
-        for(int i=0;i<k;i++){
-            curr*=10;
-            curr+=(s[i]-'0');
-        }
+        // for(int i=0;i<k;i++){
+        //     curr*=10;
+        //     curr+=(s[i]-'0');
+        // }
         int mod=pow(10,k-1);
-        if(curr!=0 && num%curr==0)
-            ans++;
-        for(int i=k;i<s.size();i++){
-            curr%=mod;
+        // if(curr!=0 && num%curr==0)
+        //     ans++;
+        for(int i=0;i<s.size();i++){
+            if(i>=k)
+                curr%=mod;
             curr*=10;
             curr+=(s[i]-'0');
-            if(curr!=0 && num%curr==0)
+            if(i>=k-1 && curr!=0 && num%curr==0)
                 ans++;
         }
         return ans;
