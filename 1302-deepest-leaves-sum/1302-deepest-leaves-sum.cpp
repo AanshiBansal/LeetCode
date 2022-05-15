e8c63ac078cbfd81adc31ea9078f4c866c525c3c
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-15 11:37:51 +0530

    Time: 112 ms (78.05%), Space: 59.7 MB (94.18%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -10,29 +10,25 @@
  * };
  */
 class Solution {
-    void inorder(TreeNode* root, int curr, int &depth){
-        if(root == NULL){
-            depth=max(depth,curr);
-            return;
-        }
-        inorder(root->left,curr+1,depth);
-        inorder(root->right,curr+1,depth);
-    }
-    void helper(TreeNode* root, int curr,int depth, int &ans){
+    void inorder(TreeNode* root, int curr, int &maxDepth, int &ans){
         if(root == NULL){
             return;
         }
-        if(curr==depth)
+        if(curr>maxDepth){
+            ans=0;
+            maxDepth=curr;
+        }
+        if(curr==maxDepth){
             ans+=root->val;
-        helper(root->left,curr+1,depth,ans);
-        helper(root->right,curr+1,depth,ans);
+        }
+        inorder(root->left,curr+1,maxDepth,ans);
+        inorder(root->right,curr+1,maxDepth,ans);
     }
 public:
     int deepestLeavesSum(TreeNode* root) {
-        int depth=0;
-        inorder(root,0,depth);
+        int maxDepth=0;
         int ans=0;
-        helper(root,1,depth,ans);
+        inorder(root,0,maxDepth,ans);
         return ans;
     }
 };
