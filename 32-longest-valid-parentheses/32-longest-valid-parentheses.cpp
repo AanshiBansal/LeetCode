1da65f49600a7cd8985312f53375179fa00d8ff5
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-24 23:40:23 +0530

    Time: 7 ms (47.05%), Space: 6.9 MB (88.01%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,21 +1,53 @@
+// class Solution {
+// public:
+//     int longestValidParentheses(string s) {
+//         stack<int>st;
+//         int ans=0;
+//         st.push(-1);
+//         for(int i=0;i<s.size();i++){
+//             if(s[i]=='('){
+//                 st.push(i);
+//             }else{
+//                 st.pop();
+//                 if(st.empty()){
+//                     st.push(i);
+//                 }else{
+//                     ans=max(ans,i-st.top());
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
-public:
-    int longestValidParentheses(string s) {
-        stack<int>st;
-        int ans=0;
-        st.push(-1);
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                st.push(i);
-            }else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
-                }else{
-                    ans=max(ans,i-st.top());
-                }
+    public: int longestValidParentheses(string s) {
+        int left = 0, right = 0, maxlength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = max(maxlength, 2 * right);
+            } else if (right > left) {
+                left = right = 0;
             }
         }
-        return ans;
+        left = right = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s[i] == '(') {
+                left++;
+            } else {
+                right++;
+            }
+            if (left == right) {
+                maxlength = max(maxlength, 2 * left);
+            } else if (left > right) {
+                left = right = 0;
+            }
+        }
+        return maxlength;
     }
 };
