2826015ed2cfa47b3ce3a7f9195c0e4158eb5b61
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-26 12:01:08 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,10 +1,22 @@
+// class Solution {
+// public:
+//     int hammingWeight(uint32_t n) {
+//         int ans=0;
+//         for(int i=0;i<32;i++){
+//             if((n>>i)&1)
+//                 ans++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int ans=0;
-        for(int i=0;i<32;i++){
-            if((n>>i)&1)
-                ans++;
+        while(n){
+            ans+=n%2;
+            n/=2;
         }
         return ans;
     }
