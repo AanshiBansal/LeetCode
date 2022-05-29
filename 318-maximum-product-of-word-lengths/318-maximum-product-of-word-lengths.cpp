9fcedfe86dfb3d506a7750fb8afee9f228666d41
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-05-29 17:57:43 +0530

    Time: 73 ms (54.24%), Space: 15.5 MB (89.46%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,24 +1,48 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<string>& words) {
+//         int n=words.size();
+//         vector<vector<bool>>count(n,vector<bool>(26,0));
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<words[i].size();j++){
+//                 count[i][words[i][j]-'a']=true;
+//             }
+//         }
+//         unsigned long ans=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 bool flag=true;
+//                 for(int k=0;k<26;k++){
+//                     if(count[i][k] && count[j][k]){
+//                         flag=false;
+//                         break;
+//                     }
+//                 }
+//                 if(flag){
+//                     ans=max(ans,words[i].size()*words[j].size());
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
         int n=words.size();
-        vector<vector<bool>>count(n,vector<bool>(26,0));
+        vector<int>bitValue(n);
         for(int i=0;i<n;i++){
+            int value=0;
             for(int j=0;j<words[i].size();j++){
-                count[i][words[i][j]-'a']=true;
+                value|=(1<<(words[i][j]-'a'));
             }
+            bitValue[i]=value;
         }
         unsigned long ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                bool flag=true;
-                for(int k=0;k<26;k++){
-                    if(count[i][k] && count[j][k]){
-                        flag=false;
-                        break;
-                    }
-                }
-                if(flag){
+                if(!((bitValue[i]&bitValue[j])>0)){
                     ans=max(ans,words[i].size()*words[j].size());
                 }
             }
