0f5d6d98b367ac8b4c3d207243c4592e92ef2681
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-14 23:30:02 +0530

    Time: 84 ms (47.20%), Space: 31.1 MB (76.06%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,39 +1,53 @@
+// class Solution {
+// public:
+//     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+//         int diff=abs(arr[0]-x);
+//         int a=-1,b=0;
+//         vector<int>ans;
+//         for(int i=1;i<arr.size();i++){
+//             if(abs(arr[i]-x)<=diff){
+//                 a=i-1;
+//                 b=i;
+//                 diff=abs(arr[i]-x);
+//             }else{
+//                 break;
+//             }
+//         }
+//         while(k--){
+//             if(a<0){
+//                 ans.push_back(arr[b]);
+//                 b++;
+//                 continue;
+//             }
+//             if(b==arr.size()){
+//                 ans.push_back(arr[a]);
+//                 a--;
+//                 continue;
+//             }
+//             if(abs(arr[a]-x)<=abs(arr[b]-x)){
+//                 ans.push_back(arr[a]);
+//                 a--;
+//             }else{
+//                 ans.push_back(arr[b]);
+//                 b++;
+//             }
+//         }
+//         sort(ans.begin(),ans.end());
+//         return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int diff=abs(arr[0]-x);
-        int a=-1,b=0;
-        vector<int>ans;
-        for(int i=1;i<arr.size();i++){
-            if(abs(arr[i]-x)<=diff){
-                a=i-1;
-                b=i;
-                diff=abs(arr[i]-x);
-            }else{
-                break;
-            }
+        int i=0,j=arr.size()-1;
+        while((j-i+1)>k){
+            if(abs(arr[i]-x)<=abs(arr[j]-x))
+                j--;
+            else
+                i++;
         }
-        //cout<<a<<" "<<b<<endl;
-        while(k--){
-            if(a<0){
-                ans.push_back(arr[b]);
-                b++;
-                continue;
-            }
-            if(b==arr.size()){
-                ans.push_back(arr[a]);
-                a--;
-                continue;
-            }
-            if(abs(arr[a]-x)<=abs(arr[b]-x)){
-                ans.push_back(arr[a]);
-                a--;
-            }else{
-                ans.push_back(arr[b]);
-                b++;
-            }
-        }
-        sort(ans.begin(),ans.end());
-        return ans;
+        return vector<int>(arr.begin()+i,arr.begin()+j+1);
     }
 };
