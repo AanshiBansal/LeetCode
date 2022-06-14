13ffcc72ad21a7a082b90fb87293230ab50be844
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-14 18:49:19 +0530

    Time: 17 ms (77.81%), Space: 6.8 MB (97.73%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -4,18 +4,20 @@ public:
         int m=word1.size();
         int n=word2.size();
         vector<int>prev(n+1,0);
+        for(int i=0;i<=n;i++)
+            prev[i]=i;
         vector<int>curr(n+1);
-        curr[0]=0;
         for(int i=1;i<=m;i++){
+            curr[0]=i;
             for(int j=1;j<=n;j++){
                 if(word1[i-1]==word2[j-1]){
-                    curr[j]=prev[j-1]+1;
+                    curr[j]=prev[j-1];
                 }else{
-                    curr[j]=max(prev[j],curr[j-1]);
+                    curr[j]=min(prev[j],curr[j-1])+1;
                 }
             }
             prev=curr;
         }
-        return m+n-curr[n]-curr[n];
+        return curr[n];
     }
 };
