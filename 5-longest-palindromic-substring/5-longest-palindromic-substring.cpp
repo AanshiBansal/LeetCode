1a29e66edb578332709bafb376b94648a21f6a4e
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-16 13:56:29 +0530

    Time: 1214 ms (7.19%), Space: 37 MB (29.01%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,22 +1,53 @@
+// class Solution {
+// private:
+//     int isPalindrome(string &s, int i, int j,vector<vector<int>>&dp){
+//         if(i>=j)
+//             return 1;
+//         if(dp[i][j]!=-1)
+//             return dp[i][j];
+//         if(s[i]!=s[j])
+//             return dp[i][j]=0;
+//         return dp[i][j]=isPalindrome(s,i+1,j-1,dp);
+//     }
+// public:
+//     string longestPalindrome(string s) {
+//         string ans="";
+//         int n=s.size();
+//         vector<vector<int>>dp(n,vector<int>(n,-1));
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 if(isPalindrome(s,i,j,dp)==1){
+//                     if(j-i+1>ans.size())
+//                         ans=s.substr(i,j-i+1);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
-private:
-    int isPalindrome(string &s, int i, int j,vector<vector<int>>&dp){
-        if(i>=j)
-            return 1;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(s[i]!=s[j])
-            return dp[i][j]=0;
-        return dp[i][j]=isPalindrome(s,i+1,j-1,dp);
-    }
 public:
     string longestPalindrome(string s) {
         string ans="";
         int n=s.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<bool>>dp(n,vector<bool>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++)
+                dp[i][j]=true;
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j]){
+                    dp[i][j]=dp[i+1][j-1];
+                }else{
+                    dp[i][j]=false;
+                }
+            }
+        }
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j,dp)==1){
+                if(dp[i][j]){
                     if(j-i+1>ans.size())
                         ans=s.substr(i,j-i+1);
                 }
@@ -25,3 +56,4 @@ public:
         return ans;
     }
 };
+
