879d805a2598c16fd79497a8630f89d2d6193f82
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-16 14:03:43 +0530

    Time: 1532 ms (5.01%), Space: 47.6 MB (27.36%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,33 @@
-// class Solution {
-// private:
-//     int isPalindrome(string &s, int i, int j,vector<vector<int>>&dp){
-//         if(i>=j)
-//             return 1;
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
-//         if(s[i]!=s[j])
-//             return dp[i][j]=0;
-//         return dp[i][j]=isPalindrome(s,i+1,j-1,dp);
-//     }
-// public:
-//     string longestPalindrome(string s) {
-//         string ans="";
-//         int n=s.size();
-//         vector<vector<int>>dp(n,vector<int>(n,-1));
-//         for(int i=0;i<n;i++){
-//             for(int j=i;j<n;j++){
-//                 if(isPalindrome(s,i,j,dp)==1){
-//                     if(j-i+1>ans.size())
-//                         ans=s.substr(i,j-i+1);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
+// DB-Memorization
+/*
+class Solution {
+private:
+    int isPalindrome(string &s, int i, int j,vector<vector<int>>&dp){
+        if(i>=j)
+            return 1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(s[i]!=s[j])
+            return dp[i][j]=0;
+        return dp[i][j]=isPalindrome(s,i+1,j-1,dp);
+    }
+public:
+    string longestPalindrome(string s) {
+        string ans="";
+        int n=s.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPalindrome(s,i,j,dp)==1){
+                    if(j-i+1>ans.size())
+                        ans=s.substr(i,j-i+1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+*/
 
 // DB-Tabulation
 class Solution {
@@ -50,14 +53,6 @@ public:
                 }
             }
         }
-        // for(int i=0;i<n;i++){
-        //     for(int j=i;j<n;j++){
-        //         if(dp[i][j]){
-        //             if(j-i+1>ans.size())
-        //                 ans=s.substr(i,j-i+1);
-        //         }
-        //     }
-        // }
         return ans;
     }
 };
