3c321e445bb2961b822a83de6ec709404d65816b
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-17 14:11:38 +0530

    Time: 39 ms (8.23%), Space: 28.1 MB (5.10%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.cpp
+++ 968-binary-tree-cameras/968-binary-tree-cameras.cpp
@@ -3,23 +3,24 @@
 // 2 -> cover for parent required
 class Solution {
 private:
-    int fn(TreeNode* root, int state, vector<unordered_map<TreeNode*,int>>&mp){
+    vector<unordered_map<TreeNode*,int>>mp;
+    int fn(TreeNode* root, int state){
         if(state==2 && root==NULL)
             return 1;
         if(root==NULL)
             return 0;
         if(mp[state].find(root)!=mp[state].end())
             return mp[state][root];
-        int ans=1+fn(root->left,0,mp)+fn(root->right,0,mp);
+        int ans=1+fn(root->left,0)+fn(root->right,0);
         if(state==0)
-            return mp[state][root]=min(ans,fn(root->left,1,mp)+fn(root->right,1,mp));
+            return mp[state][root]=min(ans,fn(root->left,1)+fn(root->right,1));
         if(state==1)
-            return mp[state][root]=min(ans,min(fn(root->left,1,mp)+fn(root->right,2,mp),fn(root->left,2,mp)+fn(root->right,1,mp)));
+            return mp[state][root]=min(ans,min(fn(root->left,1)+fn(root->right,2),fn(root->left,2)+fn(root->right,1)));
         return mp[state][root]=ans;
     }
 public:
     int minCameraCover(TreeNode* root) {
-        vector<unordered_map<TreeNode*,int>>mp(3);
-        return fn(root,1,mp);
+        mp=vector<unordered_map<TreeNode*,int>>(3);
+        return fn(root,1);
     }
 };
