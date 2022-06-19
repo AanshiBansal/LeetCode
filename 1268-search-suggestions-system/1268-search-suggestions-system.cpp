48f1badbd0b13bfbbe584ea4db39ca46d2912a5e
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-19 12:09:25 +0530

    Time: 98 ms (67.07%), Space: 32 MB (68.14%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -3,9 +3,11 @@ public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
         sort(products.begin(),products.end());
         vector<vector<string>>ans;
+        int start=0;
         for(int i=0;i<searchWord.size();i++){
             string curr=searchWord.substr(0,i+1);
-            int index=lower_bound(products.begin(),products.end(),curr)-products.begin();
+            int index=lower_bound(products.begin()+start,products.end(),curr)-products.begin();
+            start=index;
             int count=3;
             vector<string>r;
             while(index<products.size()&&count--){
