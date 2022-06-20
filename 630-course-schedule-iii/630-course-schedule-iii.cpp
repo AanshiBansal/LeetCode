00ad0d027c88eb1fef2c29748f1a13ad84bb3e9b
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-20 19:38:45 +0530

    Time: 368 ms (42.22%), Space: 56.3 MB (56.79%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,25 @@
+bool comparator(vector<int>&a, vector<int>&b){
+    return a[1]<b[1];
+}
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(),courses.end(),comparator);
+        priority_queue<int>pq;
+        int time=0;
+        for(int i=0;i<courses.size();i++){
+            if(time+courses[i][0]<=courses[i][1]){
+                pq.push(courses[i][0]);
+                time+=courses[i][0];
+            }else{
+                if(!pq.empty() && pq.top()>courses[i][0]){
+                    time-=pq.top();
+                    pq.pop();
+                    pq.push(courses[i][0]);
+                    time+=courses[i][0];
+                }
+            }
+        }
+        return pq.size();
+    }
+};
