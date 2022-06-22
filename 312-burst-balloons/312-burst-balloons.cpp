bfe8629c39091bff00e7b8b3d094f86cf97074ab
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-22 19:54:23 +0530

    Time: 534 ms (62.78%), Space: 10.3 MB (52.15%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -1,31 +1,4 @@
 // class Solution {
-// public:
-//     int maxCoins(vector<int>& nums) {
-//         int n=nums.size();
-//         if(n==1)
-//             return nums[0];
-//         int ans=0;
-//         vector<pair<int,int>>lr(n);
-//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-//         for(int i=1;i<n-1;i++){
-//             lr[i]={i-1,i+1};
-//             pq.push({nums[i],i});
-//         }
-//         while(!pq.empty()){
-//             int index=pq.top().second;
-//             pq.pop();
-//             cout<<index<<" "<<lr[index].first<<" "<<lr[index].second<<endl;
-//             lr[lr[index].first].second=lr[index].second;
-//             lr[lr[index].second].first=lr[index].first;
-//             ans+=(nums[index]*nums[lr[index].first]*nums[lr[index].second]);
-//             //cout<<ans<<endl;
-//         }
-//         ans+=(max(nums[0],nums[n-1])*(min(nums[0],nums[n-1])+1));
-//         return ans;
-//     }
-// };
-
-// class Solution {
 // private:
 //     int fn(vector<int>& nums, int start, int end, vector<vector<int>>&dp){
 //         if(start>end)
