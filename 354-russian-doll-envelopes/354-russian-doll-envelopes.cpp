e577e6682ac3a88adbeda86c4c73e21b2a0f7b65
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-23 01:43:32 +0530

    Time: 605 ms (35.37%), Space: 77.4 MB (90.81%) - LeetHub

--- 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.cpp
@@ -69,9 +69,7 @@
 // };
 
 bool comp(vector<int>&a, vector<int>&b){
-    if(a[0]<b[0])
-        return true;
-    if(a[0]==b[0] && a[1]>b[1])
+    if(a[0]<b[0] || (a[0]==b[0] && a[1]>b[1]))
         return true;
     return false;
 }
