357e52c2f3f3e1081f343e18bf9348bf67bb37eb
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-06-26 13:37:58 +0530

    Time: 27 ms (36.40%), Space: 10.8 MB (35.60%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,14 +1,32 @@
+// class Solution {
+// public:
+//     bool carPooling(vector<vector<int>>& trips, int capacity) {
+//         vector<int>dist(1001,0);
+//         for(int i=0;i<trips.size();i++){
+//             dist[trips[i][1]]+=trips[i][0];
+//             dist[trips[i][2]]-=trips[i][0];
+//         }
+//         int curr=0;
+//         for(int i=0;i<=1000;i++){
+//             curr+=dist[i];
+//             if(curr>capacity)
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<int>dist(1001,0);
+        map<int,int>mp;
         for(int i=0;i<trips.size();i++){
-            dist[trips[i][1]]+=trips[i][0];
-            dist[trips[i][2]]-=trips[i][0];
+            mp[trips[i][1]]+=trips[i][0];
+            mp[trips[i][2]]-=trips[i][0];
         }
         int curr=0;
-        for(int i=0;i<=1000;i++){
-            curr+=dist[i];
+        for(auto p:mp){
+            curr+=p.second;
             if(curr>capacity)
                 return false;
         }
