ee7665df2f3dbc0163425cc5d531631b378a13f7
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-02 19:07:17 +0530

    Time: 199 ms (79.05%), Space: 96.3 MB (98.03%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,25 +1,40 @@
+// class Solution {
+// public:
+//     bool isMonotonic(vector<int>& nums) {
+//         if(nums.size()<=2)
+//             return true;
+//         int i=0;
+//         while(i+1<nums.size() && nums[i]==nums[i+1]){
+//             i++;
+//         }
+//         if(i==nums.size()-1)
+//             return true;
+//         if(nums[i+1]>nums[i]){
+//             for(;i<nums.size()-1;i++){
+//                 if(nums[i]>nums[i+1])
+//                     return false;
+//             }
+//             return true;
+//         }
+//         for(;i<nums.size()-1;i++){
+//             if(nums[i]<nums[i+1])
+//                 return false;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if(nums.size()<=2)
-            return true;
-        int i=0;
-        while(i+1<nums.size() && nums[i]==nums[i+1]){
-            i++;
-        }
-        if(i==nums.size()-1)
-            return true;
-        if(nums[i+1]>nums[i]){
-            for(;i<nums.size()-1;i++){
-                if(nums[i]>nums[i+1])
-                    return false;
-            }
-            return true;
-        }
-        for(;i<nums.size()-1;i++){
+        bool increasing=true;
+        bool decreasing=true;
+        for(int i=0;i<nums.size()-1;i++){
             if(nums[i]<nums[i+1])
-                return false;
+                decreasing=false;
+            if(nums[i]>nums[i+1])
+                increasing=false;
         }
-        return true;
+        return increasing || decreasing;
     }
 };
