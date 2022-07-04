d23a7bf5f117e17d03e281bff3abb296f781c114
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-04 17:22:55 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (61.60%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
+    int robI(vector<int>nums) {
+        int n=nums.size();
+        nums[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+            nums[i]=max(nums[i-1],nums[i-2]+nums[i]);
+        return nums[n-1];
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0)
             return 0;
         if(n==1)
             return nums[0];
-        vector<int>dp=nums;
-        dp[0]=0;
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
+        int last=nums[n-1];
         nums[n-1]=0;
-        nums[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++)
-            nums[i]=max(nums[i-1],nums[i-2]+nums[i]);
-        return max(nums[n-1],dp[n-1]);
+        int ans=robI(nums);
+        nums[n-1]=last;
+        nums[0]=0;
+        ans=max(ans,robI(nums));
+        return ans;
     }
 };
