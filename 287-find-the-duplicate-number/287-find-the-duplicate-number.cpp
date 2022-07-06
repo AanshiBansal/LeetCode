699e0fd3e20e311aab3875e24ef5d039d3193f11
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-06 18:57:49 +0530

    Time: 232 ms (27.84%), Space: 61.4 MB (27.90%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,17 +1,39 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int tortoise=nums[0];
+//         int hare=nums[0];
+//         do{
+//             hare=nums[nums[hare]];
+//             tortoise=nums[tortoise];
+//         }while(hare!=tortoise);
+//         hare=nums[0];
+//         while(hare!=tortoise){
+//             hare=nums[hare];
+//             tortoise=nums[tortoise];
+//         }
+//         return tortoise;
+//     }
+// };
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int tortoise=nums[0];
-        int hare=nums[0];
-        do{
-            hare=nums[nums[hare]];
-            tortoise=nums[tortoise];
-        }while(hare!=tortoise);
-        hare=nums[0];
-        while(hare!=tortoise){
-            hare=nums[hare];
-            tortoise=nums[tortoise];
+        int low=1;
+        int high=nums.size()-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            int count=0;
+            for(int num:nums){
+                if(num<=mid)
+                    count++;
+            }
+            if(count>mid){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
         }
-        return tortoise;
+        return low;
     }
 };
