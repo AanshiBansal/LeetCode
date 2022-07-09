baa6ac154fd0fcaaf11b8363608c51b3736a93c0
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-09 23:36:10 +0530

    Time: 401 ms (50.00%), Space: 66.3 MB (50.00%) - LeetHub

---
+++ 2332-the-latest-time-to-catch-a-bus/2332-the-latest-time-to-catch-a-bus.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int latestTimeCatchTheBus(vector<int>& buses, vector<int>& passengers, int capacity) {
+        int n=buses.size();
+        sort(buses.begin(),buses.end());
+        sort(passengers.begin(),passengers.end());
+       
+        int index=0;
+        int last=-1;
+        int count=0;
+        for(int i=0;i<n;i++){
+            count=0;
+            last=-1;
+            while(count<capacity && index<passengers.size() && passengers[index]<=buses[i]){
+                count++;
+                last=index;
+                index++;
+            }
+        }
+        if(last==-1 || last==passengers.size() || (count<capacity && passengers[last]!=buses[n-1]))
+            return buses[n-1];
+        
+        while(last-1>=0 && passengers[last]-1==passengers[last-1]){
+            last--;
+        }
+        return passengers[last]-1;
+        
+    }
+};
