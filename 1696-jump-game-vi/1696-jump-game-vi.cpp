99737b918c5e1e794816552da5af74311ce3bf10
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-10 19:50:11 +0530

    Time: 695 ms (5.25%), Space: 132.3 MB (22.31%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        if(nums.size()==1)return nums[0];
-        vector<int>dp(nums.size(),INT_MIN);
+        if(nums.size()==1)
+            return nums[0];
+        
         multiset<int>Q;
-        dp[0] = nums[0];
-        Q.insert(dp[0]);
+        Q.insert(nums[0]);
+        
         for(int i = 1;i<nums.size();i++){
             if(Q.size()>k){
-                auto it = Q.find(dp[i-k-1]);
+                auto it = Q.find(nums[i-k-1]);
                 Q.erase(it);
             }
-            dp[i] = *Q.rbegin()+nums[i];
-            Q.insert(dp[i]);
+            nums[i] += *Q.rbegin();
+            Q.insert(nums[i]);
         }
-        return dp[nums.size()-1];
+        
+        return nums[nums.size()-1];
     }
 };
