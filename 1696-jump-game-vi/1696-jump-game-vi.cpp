186e5a4f2857eaa337955cdb1f29b4835b5cee04
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-10 20:14:54 +0530

    Time: 178 ms (97.47%), Space: 78.5 MB (83.60%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,21 +1,42 @@
+// class Solution {
+// public:
+//     int maxResult(vector<int>& nums, int k) {
+//         if(nums.size()==1)
+//             return nums[0];
+        
+//         multiset<int>Q;
+//         Q.insert(nums[0]);
+        
+//         for(int i = 1;i<nums.size();i++){
+//             if(Q.size()>k){
+//                 auto it = Q.find(nums[i-k-1]);
+//                 Q.erase(it);
+//             }
+//             nums[i] += *Q.rbegin();
+//             Q.insert(nums[i]);
+//         }
+        
+//         return nums[nums.size()-1];
+//     }
+// };
+
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        if(nums.size()==1)
+        int n=nums.size();
+        if(n==1)
             return nums[0];
-        
-        multiset<int>Q;
-        Q.insert(nums[0]);
-        
-        for(int i = 1;i<nums.size();i++){
-            if(Q.size()>k){
-                auto it = Q.find(nums[i-k-1]);
-                Q.erase(it);
+        deque<int>q;
+        q.push_back(0);
+        for(int i=1;i<n;i++){
+            if(q.front()==i-k-1)
+                q.pop_front();
+            nums[i]+=nums[q.front()];
+            while(!q.empty() && nums[q.back()]<nums[i]){
+                q.pop_back();
             }
-            nums[i] += *Q.rbegin();
-            Q.insert(nums[i]);
+            q.push_back(i);
         }
-        
-        return nums[nums.size()-1];
+        return nums[n-1];
     }
 };
