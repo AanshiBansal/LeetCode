cd1e9b0113354466b7edbf8a42825445a7c33197
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-12 13:17:26 +0530

    Time: 31 ms (85.48%), Space: 10.2 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -9,9 +9,13 @@ private:
             return true;
         }
         for(int i=0;i<4;i++){
+            // pruning 4
+            // if same length of sides then we can skip
             if(i>0 && sides[i]==sides[i-1])
                 continue;
             sides[i]+=matchsticks[index];
+            // pruning 5
+            // if side's length exceeds length of square, no need to proceed further
             if(sides[i]<=length && rec(matchsticks,index+1,sides,length))
                 return true;
             sides[i]-=matchsticks[index];
@@ -24,11 +28,15 @@ public:
         int sum=0;
         for(int s:matchsticks)
             sum+=s;
+        // pruning 1
         if(sum%4 !=0)
             return false;
+        // pruning 2
         for(int s:matchsticks)
             if(s>sum/4)
                 return false;
+        // pruning 3
+        // put longer matchsticks first - shortens tree
         sort(matchsticks.begin(),matchsticks.end(),greater());
         return rec(matchsticks,0,sides,sum/4);
     }
