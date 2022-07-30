cb07e2fbf2c399a99df1086172d5f94145346aa5
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-07-30 13:06:39 +0530

    Time: 304 ms (56.18%), Space: 102.2 MB (73.71%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -4,12 +4,8 @@ public:
         vector<string>ans;
         int m=words1.size();
         int n=words2.size();
-        vector<vector<int>>mp1(m,vector<int>(26,0));
+        
         vector<int>mp2(26,0);
-        for(int i=0;i<m;i++){
-            for(auto c:words1[i])
-                mp1[i][c-'a']++;
-        }
         for(int i=0;i<n;i++){
             vector<int>temp(26,0);
             for(auto c:words2[i]){
@@ -18,10 +14,14 @@ public:
             for(int j=0;j<26;j++)
                 mp2[j]=max(mp2[j],temp[j]);
         }
+        
         for(int i=0;i<m;i++){
             bool flag=true;
+            vector<int>mp1(26,0);
+            for(auto c:words1[i])
+                mp1[c-'a']++;
             for(int k=0;k<26;k++){
-                if(mp1[i][k]<mp2[k]){
+                if(mp1[k]<mp2[k]){
                     flag=false;
                     break;
                 }
