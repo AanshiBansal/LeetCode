9f16e095245bbdf97ce026def95e47f0b7b91e90
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-08-19 23:48:18 +0530

    Time: 131 ms (72.37%), Space: 54.4 MB (98.23%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -3,11 +3,6 @@ public:
     bool isPossible(vector<int>& nums) {
         int p1=0,p2=0,p3=0;
         for(int i=0;i<nums.size();i++){
-            if(i>0 && nums[i-1]!=nums[i]-1){
-                if(p1!=0 || p2!=0)
-                    return false;
-                p1=0;p2=0;p3=0;
-            }
             int count=1;
             while(i+1<nums.size() && nums[i+1]==nums[i]){
                 count++;
@@ -19,10 +14,12 @@ public:
             int c3=p2+min(p3,count-p1-p2);
             int c1=count-c2-c3;
             p1=c1;p2=c2;p3=c3;
-            //cout<<nums[i]<<" "<<p1<<" "<<p2<<" "<<p3<<endl;
+            if(i+1==nums.size() || nums[i+1]!=nums[i]+1){
+                if(p1!=0 || p2!=0)
+                    return false;
+                p1=0;p2=0;p3=0;
+            }
         }
-        if(p1!=0 || p2!=0)
-            return false;
         return true;
     }
 };
