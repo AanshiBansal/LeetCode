28447709ac10ecdcebd78ac7117c21c51a12ed8e
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-08-19 23:13:45 +0530

    Time: 139 ms (66.47%), Space: 56.2 MB (79.10%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>prev;
+        for(int i=0;i<nums.size();i++){
+            int count=1;
+            if(i>0 && nums[i]!=nums[i-1]+1){
+                if(!prev.empty() && prev.top()<3){
+                    return false;
+                }
+                prev=priority_queue<int,vector<int>,greater<int>>();
+            }
+            while(i+1<nums.size() && nums[i]==nums[i+1]){
+                count++;
+                i++;
+            }
+            priority_queue<int,vector<int>,greater<int>>curr;
+            while(!prev.empty() && count>0){
+                curr.push(prev.top()+1);
+                prev.pop();
+                count--;
+            }
+            if(!prev.empty() && prev.top()<3){
+                return false;
+            }
+            while(count>0){
+                curr.push(1);
+                count--;
+            }
+            prev=curr;
+        }
+        if(!prev.empty() && prev.top()<3){
+            return false;
+        }
+        return true;
+    }
+};
