81802f56e17c3abc7b6eac2fafe4e47b1d03cce4
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-08-24 23:26:23 +0530

    Time: 27 ms (73.41%), Space: 5.9 MB (72.69%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if(n<1)
             return false;
-        while(n>1 && n%3 == 0){
+        while(n%3 == 0){
             n/=3;
         }
         return n==1;
