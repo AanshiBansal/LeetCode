bae31ebc922525ac711a29f818872be48052acbb
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-09-04 12:54:56 +0530

    Time: 10 ms (44.99%), Space: 11.9 MB (97.44%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<pair<int,int>,vector<int>>mp;
+        queue<pair<TreeNode*, pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            int r=q.front().second.first;
+            int c=q.front().second.second;
+            //cout<<r<<" "<<c<<endl;
+            TreeNode* node=q.front().first;
+            q.pop();
+            mp[{r,c}].push_back(node->val);
+            if(node->left!=NULL){
+                q.push({node->left,{r+1,c-1}});
+            }
+            if(node->right!=NULL){
+                q.push({node->right,{r+1,c+1}});
+            }
+        }
+        for(auto itr=mp.begin();itr!=mp.end();itr++){
+            sort(itr->second.begin(),itr->second.end());
+        }
+        map<int,vector<int>>mp2;
+        //vector<vector<int>>ans(r_max+1,vector<int>());
+        //cout<<"here"<<endl;
+        for(auto itr=mp.begin();itr!=mp.end();itr++){
+            int c=itr->first.second;
+            //cout<<c<<endl;
+            if(mp2[c].size()==0)
+                mp2[c]=itr->second;
+            else
+                mp2[c].insert(mp2[c].end(),itr->second.begin(),itr->second.end());
+        }
+        vector<vector<int>>ans;
+        for(auto itr=mp2.begin();itr!=mp2.end();itr++){
+            ans.push_back(itr->second);
+        }
+        return ans;
+    }
+};
