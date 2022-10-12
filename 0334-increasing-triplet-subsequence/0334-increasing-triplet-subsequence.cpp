655a3c11361ddf3054133519016c716ae07d0b31
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-10-12 16:12:08 +0530

    Time: 83 ms (83.73%), Space: 61.7 MB (24.07%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -9,7 +9,9 @@ public:
                 v.push_back(num);
             else
                 *it=num;
+            if(v.size()==3)
+                return true;
         }
-        return v.size()>=3;
+        return false;
     }
 };
