a53c9a1fc3ebf6ff7921be984f8aa6bc9b8fe79b
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-11-01 15:12:10 +0530

    Time: 44 ms (70.36%), Space: 13.1 MB (95.61%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -4,36 +4,23 @@ public:
     vector<int> findBall(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<int>ans(n,-1);
-        queue<pr>q;
-        for(int j=0;j<n;j++){
-            if(grid[0][j]==1 && j+1!=n && grid[0][j+1]==1){
-                q.push({j+1,j});
-            }
-            if(grid[0][j]==-1 && j-1!=-1 && grid[0][j-1]==-1){
-                q.push({j-1,j});
-            }
-        }
-        int row=0;
-        while(row+1<m && q.size()>0){
-            int size=q.size();
-            for(int x=0;x<size;x++){
-                int col=q.front().first;
-                int ball=q.front().second;
-                q.pop();
+        vector<int>ans(n);
+        for(int j=0;j<n;j++)
+            ans[j]=j;
+        int row=-1;
+        while(row+1<m){
+            for(int ball=0;ball<n;ball++){
+                if(ans[ball]==-1)
+                    continue;
+                int col=ans[ball];
                 if(grid[row+1][col]==1 && col+1!=n && grid[row+1][col+1]==1)
-                    q.push({col+1,ball});
-                if(grid[row+1][col]==-1 && col-1!=-1 && grid[row+1][col-1]==-1)
-                    q.push({col-1,ball});
-                
+                    ans[ball]=col+1;
+                else if(grid[row+1][col]==-1 && col-1!=-1 && grid[row+1][col-1]==-1)
+                    ans[ball]=col-1;
+                else
+                    ans[ball]=-1;
             }
             row++;
-        }
-        while(!q.empty()){
-            int col=q.front().first;
-            int ball=q.front().second;
-            q.pop();
-            ans[ball]=col;
         }
         return ans;
     }
