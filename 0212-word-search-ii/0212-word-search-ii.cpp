2774705d2b2036f15d4d97428773cb440fe76265
Aanshi Bansal <aanshi.bansal@gmail.com>
2022-11-05 16:24:01 +0530

    Time: 1575 ms (38.20%), Space: 17 MB (10.11%) - LeetHub

---
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -0,0 +1,63 @@
+class TrieNode{
+public:
+    string word;
+    vector<TrieNode*>children;
+    TrieNode(){
+        children=vector<TrieNode*>(26,NULL);
+        word="";
+    }
+};
+class Trie{
+public:
+    TrieNode* root;
+    Trie(){
+        root=new TrieNode();
+    }
+    void addWord(string word){
+        TrieNode* curr=root;
+        for(int i=0;i<word.size();i++){
+            if(curr->children[word[i]-'a'] == NULL)
+                curr->children[word[i]-'a']=new TrieNode();
+            curr=curr->children[word[i]-'a'];
+        }
+        curr->word=word;
+    }
+};
+class Solution {
+private:
+    Trie* trie;
+    vector<vector<char>> board;
+    vector<string>ans;
+    int m,n;
+    void dfs(int i,int j,TrieNode* root){
+        if(i<0 || i==m || j<0 || j==n || board[i][j]== '#' || root->children[board[i][j]-'a'] == NULL)
+            return;
+        char c=board[i][j];
+        root = root->children[c-'a'];
+        if(root->word!=""){
+            ans.push_back(root->word);
+            root->word="";
+        }
+        board[i][j]='#';
+        dfs(i+1,j,root);
+        dfs(i-1,j,root);
+        dfs(i,j+1,root);
+        dfs(i,j-1,root);
+        board[i][j]=c;
+    }
+public:
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+        trie= new Trie();
+        this->board=board;
+        for(string w:words)
+            trie->addWord(w);
+        m=board.size();
+        n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dfs(i,j,trie->root);
+            }
+        }
+        return ans;
+    }
+};
